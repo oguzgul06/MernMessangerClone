afaa3a5837d3a385508a22d33b9f8c020ed3c859
oguz gül <ogzgl95@outlook.com>
2021-08-12 14:09:27 +0300

    Config database with mongodb

--- messenger-mern-backend/server.js
+++ messenger-mern-backend/server.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import express from "express";
+import mongoose from "mongoose";
+import Pusher from "pusher";
+import cors from "cors";
 
-function server() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+//db config
+const mongoURI =
+  "mongodb+srv://admin:CyJEndAnxbHwJYvU@cluster0.ewhof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoURI, {
+  useCareateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-export default server
+mongoose.connection.once("open", () => {
+  console.log("DB CONNECTED");
+});
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//api routes
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+// listener
+app.listen(port, () => console.log(`listening on localhost:${port}`));

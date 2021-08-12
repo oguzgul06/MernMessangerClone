3ce1092777c1d156bcf80c3aabc2b7577cbb21e2
oguz gül <ogzgl95@outlook.com>
2021-08-12 17:07:14 +0300

    Buid get and post req-res and than integrated backend to frontend

--- messenger-mern-backend/server.js
+++ messenger-mern-backend/server.js
@@ -9,6 +9,14 @@ import mongoMessages from "./messageModel.js";
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+    appId: "1249310",
+    key: "c5e9de9f1ec832763811",
+    secret: "db4dd824f61b83739194",
+    cluster: "eu",
+    useTLS: true
+  });
+
 /* middlewares */
 app.use(express.json());
 app.use(cors());
@@ -24,6 +32,13 @@ mongoose.connect(mongoURI, {
 
 mongoose.connection.once("open", () => {
   console.log("DB CONNECTED");
+
+  const changeStream = mongoose.connection.collection('messages').watch();
+  changeStream.on('change', (change) => {
+    pusher.trigger("messages", "newMessages", {
+        'change': change
+      });
+  })
 });
 
 /* api routes */
@@ -46,6 +61,9 @@ app.get("/retrieve/conversation", (req, res) => {
     if (err) {
       res.status(500).send(err);
     } else {
+      data.sort((b, a) => {
+        return a.timestamp - b.timestamp;
+      });
       res.status(200).send(data);
     }
   });
--- messenger-mern-starter/src/App.js
+++ messenger-mern-starter/src/App.js
@@ -1,60 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
-import Message from './Message';
-import db from './firebase';
-import firebase from 'firebase'
-import FlipMove from 'react-flip-move'
-import SendIcon from '@material-ui/icons/Send'
-import IconButton from '@material-ui/core/IconButton'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import Message from "./Message";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import IconButton from "@material-ui/core/IconButton";
+import Pusher from "pusher-js";
+import axios from "./axios";
+
+const pusher = new Pusher("c5e9de9f1ec832763811", {
+  cluster: "eu",
+});
 
 function App() {
-  const [input, setInput] = useState('')
-  const [messages, setMessages] = useState([])
-  const [username, setUsername] = useState('')
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+
+  const sync = async () => {
+    await axios.get("/retrieve/conversation").then((res) => {
+      console.log(res.data);
+      setMessages(res.data);
+    });
+  };
 
   useEffect(() => {
-    db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => ({ id: doc.id, message: doc.data() })))
+    sync();
+  }, []);
+
+  useEffect(()=>{
+    const channel = pusher.subscribe('messages');
+    channel.bind('newMessage', function (data) {
+      sync()
     })
-  }, [])
+  }, [username])
 
   useEffect(() => {
-    setUsername(prompt('Please enter your name'))
-  }, [])
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    db.collection('messages').add({
-      message: input,
+    axios.post("./save/message", {
       username: username,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
+      message: input,
+      timestamp: Date.now(),
+    });
 
-    setInput('')
-  }
+    setInput("");
+  };
 
   return (
     <div className="App">
-      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="messenger logo" />
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
+        alt="messenger logo"
+      />
       <h2>Welcome {username}</h2>
 
-      <form className='app__form' >
-        <FormControl className='app__formControl' >
-          <Input className='app__input' placeholder='Enter a message...' value={input} onChange={(e) => setInput(e.target.value)} />
-          <IconButton className='app__iconButton' variant='text' color='primary' disabled={!input} onClick={sendMessage} type="submit" >
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            placeholder="Enter a message..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <IconButton
+            className="app__iconButton"
+            variant="text"
+            color="primary"
+            disabled={!input}
+            onClick={sendMessage}
+            type="submit"
+          >
             <SendIcon />
           </IconButton>
         </FormControl>
       </form>
 
       <FlipMove>
-        {
-          messages.map(({ id, message }) => (
-            <Message key={id} message={message} username={username} />
-          ))
-        }
+        {messages.map((message) => (
+          <Message key={message._id} message={message} username={username} />
+        ))}
       </FlipMove>
     </div>
   );
